b23a2b743a92244cafc82f8c627478d62804cf62
ikuehne <ikuehne617@gmail.com>
2017-03-28 23:37:16 -0500

    Add nice command-line option stuff.

--- Makefile
+++ Makefile
@@ -1,9 +1,8 @@
 CC=clang++
 CXX=clang++
 
-CPPFLAGS=-g $(shell llvm-config --cxxflags) -Wall -Wpedantic -std=c++14 
-LDFLAGS=$(shell llvm-config --ldflags --system-libs --libs all)
-
+CPPFLAGS=-g $(shell llvm-config --cxxflags) -Wall -Wpedantic -std=c++14
+LDFLAGS=$(shell llvm-config --ldflags --system-libs --libs all) /usr/local/Cellar/boost/1.62.0/lib/libboost_program_options.a
 COMPILER_OBJS=CodeGenerator.o Lexer.o Parser.o
 
 all: parser_demo codegen_demo kalc
--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <fcntl.h>
+#include <fstream>
 #include <iostream>
 #include <unistd.h>
 
+#include <boost/program_options.hpp>
 #include <boost/variant.hpp>
 
 #include "AST.hh"
 #include "Parser.hh"
 #include "CodeGenerator.hh"
+
+namespace opt = boost::program_options;
+
+static const int OBJFILE_MODE_BLAZEIT = 420;
 
 void handle_input(Kaleidoscope::Parser &p, Kaleidoscope::CodeGenerator &c) {
     auto e = p.parse();
@@ -15,15 +21,42 @@ void handle_input(Kaleidoscope::Parser &p, Kaleidoscope::CodeGenerator &c) {
     }
 }
 
-int main(void) {
-    Kaleidoscope::CodeGenerator codegen("my cool jit");
-    Kaleidoscope::Parser parser;
-    while (true) {
-        if (parser.reached_end()) break;
-        handle_input(parser, codegen);
+int main(int argc, char **argv) {
+    opt::options_description desc("Kaleidoscope Compiler Options");
+    desc.add_options()
+        ("help", "print usage information")
+        ("out", opt::value<std::string>(),
+            "select output file to emit object code")
+        ("in", opt::value<std::string>(), "select input file");
+    opt::positional_options_description pos;
+    pos.add("in", -1);
+
+    opt::variables_map opt_map;
+    opt::store(opt::command_line_parser(argc, argv).options(desc)
+                                                   .positional(pos)
+                                                   .run(),
+               opt_map);
+    opt::notify(opt_map);
+
+    if (!opt_map.count("help")
+      && opt_map.count("out")
+      && opt_map.count("in")) {
+        Kaleidoscope::CodeGenerator codegen("Kaleidoscope module");
+        std::ifstream infile(opt_map["in"].as<std::string>());
+        Kaleidoscope::Parser parser(&infile);
+        while (true) {
+            if (parser.reached_end()) break;
+            handle_input(parser, codegen);
+        }
+
+        int fd = open(opt_map["out"].as<std::string>().c_str(),
+                      O_RDWR | O_CREAT,
+                      OBJFILE_MODE_BLAZEIT);
+        codegen.emit_obj(fd);
+        close(fd);
+    } else {
+        std::cerr << desc << std::endl;
+        return 1;
     }
 
-    int fd = open("a.out", O_RDWR | O_CREAT);
-    codegen.emit_obj(fd);
-    close(fd);
 }
