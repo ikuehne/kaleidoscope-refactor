61d345d66bb0561f941ded913ea99e81dd02b6dc
ikuehne <ikuehne617@gmail.com>
2017-03-29 01:04:36 -0500

    Comments.

--- kalc.cpp
+++ kalc.cpp
@@ -12,8 +12,13 @@
 
 namespace opt = boost::program_options;
 
+/* Actually the privileges most compilers create object files with. */
 static const int OBJFILE_MODE_BLAZEIT = 420;
 
+/**
+ * @brief Pull a single AST out of the parser, and have the code generator
+ *        visit it.
+ */
 void handle_input(Kaleidoscope::Parser &p, Kaleidoscope::CodeGenerator &c) {
     auto e = p.parse();
     if (!Kaleidoscope::AST::is_err(e)) {
@@ -21,7 +26,11 @@ void handle_input(Kaleidoscope::Parser &p, Kaleidoscope::CodeGenerator &c) {
     }
 }
 
+/**
+ * @brief Entry point.
+ */
 int main(int argc, char **argv) {
+    /* Boost command-line option stuff... */
     opt::options_description desc("Kaleidoscope Compiler Options");
     desc.add_options()
         ("help", "print usage information")
@@ -38,23 +47,33 @@ int main(int argc, char **argv) {
                opt_map);
     opt::notify(opt_map);
 
+    /* If the user did good, */
     if (!opt_map.count("help")
       && opt_map.count("out")
       && opt_map.count("in")) {
+        /* Get a code generator. */
         Kaleidoscope::CodeGenerator codegen("Kaleidoscope module");
+        /* Open the source file. */
         std::ifstream infile(opt_map["in"].as<std::string>());
+        /* Construct a parser on that file. */
         Kaleidoscope::Parser parser(&infile);
+        /* Pull ASTs out of the parser */
         while (true) {
+            /* until we hit EOF. */
             if (parser.reached_end()) break;
             handle_input(parser, codegen);
         }
 
+        /* Open the output file (LLVM's stream formats are weird, so we can't
+         * use regular STL stream classes). */
         int fd = open(opt_map["out"].as<std::string>().c_str(),
                       O_RDWR | O_CREAT,
                       OBJFILE_MODE_BLAZEIT);
+        /* Emit the object code. */
         codegen.emit_obj(fd);
         close(fd);
     } else {
+        /* Print usage information if the user did bad. */
         std::cerr << desc << std::endl;
         return 1;
     }
